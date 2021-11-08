348cd17100e8887e062f18db04e2a43bafba04d7
khalia <khaliaparris01@gmail.com>
2021-11-08 15:23:14 +0000

    finished code challenge

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //write your code here
+
+function averageInArray(array){
+    let sum= 0;
+    for (let i = 0; i < array.length ; i++){
+        sum += array[i];
+        
+    } 
+    sum = sum/array.length;
+    return sum
+}
+console.log(averageInArray([2,2,5,10]))
